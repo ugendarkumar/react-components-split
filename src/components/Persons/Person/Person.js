1279ca9270e243ff53e2aa6330af58781503a580
Ugendarkumar J P <ugendarkumar_j@photonint.com>
2019-05-23 19:56:22 +0530

    props-types module and Ref for state and stateless components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,7 +2,46 @@ import React,{Component} from 'react';
 import './Person.css';
 import Aux from '../../../hoc/Auxillary';
 import withClass from '../../../hoc/withClass';
+import propTypes from 'prop-types';
+
+
 class Person extends Component{
+
+
+
+    constructor(props){
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+
+
+    componentDidMount(){
+     //   document.querySelector('input').focus();
+  
+    /*
+     [step - 1]
+    using "ref" property in the element passing by in a callback function with an argument which has the element which can
+    be intialized to a global property which is not like state and can be called in componentDidMount - follow this method in (step - 1)
+     here the last input element is set to focus.
+
+    */
+  // [step - 1]   this.inputElement.focus()
+    
+    /*
+    [step - 2] create a constructor function, intialize a property with built in React.createRef()
+      using the "ref" property in the element intialize to the property in constructor function
+      in componentDidMount() call the property in constructor with current property using dot-notation to get individual elements
+      
+    */
+
+    // In both methods individual elements are accessed
+
+    //[step - 2]
+    console.log(this.inputElementRef.current)
+    this.inputElementRef.current.focus()
+    }
+
     render(){
         console.log('[Person.js] rendering .............')
         
@@ -11,16 +50,25 @@ class Person extends Component{
     // from 'react' and use <Fragment> </Fragment>
 
         return (
-   <Aux>
-        <p onClick = {this.props.delete} >I am {this.props.name} and age is {this.props.age}</p>
-    <input type="text" onChange = {this.props.change} value = {this.props.name}></input>
-    </Aux>
-    )
+            <Aux>
+                <p onClick={this.props.delete} >I am {this.props.name} and age is {this.props.age}</p>
+                <input type="text"
+                    onChange={this.props.change}
+                 // [step - 1]  ref={(inputElem) => {this.inputElement = inputElem}}
+                    //[step -2]
+                    ref = {this.inputElementRef}
+                    value={this.props.name}></input>
+            </Aux>
+        )
     }
-    
 }
 
-  
+Person.propTypes = {
+    change:propTypes.func,
+    delete:propTypes.func,
+    age:propTypes.number,
+    name:propTypes.string
+}  
 
 
 export default withClass(Person,"Person");
--- src/components/cockpit/Cockpit.js
+++ src/components/cockpit/Cockpit.js
@@ -1,16 +1,26 @@
-import React,{useEffect} from 'react';
+import React,{useEffect,useRef} from 'react';
 
 import './Cockpit.css';
 
 
 
 const Cockpit = props => {
+
+  const toggleBtnRef = useRef(null);
+
+
+  /* Ref can be used to access the element in JSX. In functional components there is useRef hook
+  which is intialized to a variable which gets null as a default argument and it's set to ref property to an
+  element (here in toggle button) in useEffect (after jsx is rendered) this is called with click event with "current" property using 
+  dot-notation to access the input field .
+ */
   useEffect(() => {
    console.log('[Cockpit.js] useEffect');
    
-   setTimeout(() => {
-     alert('get data from cloud');
-   },1000)
+  //  setTimeout(() => {
+  //    alert('get data from cloud');
+  //  },1000)
+  toggleBtnRef.current.click()
   return () => {
     console.log('[Cockpit.js] clean up work in useEffect')
   }
@@ -44,7 +54,7 @@ if(props.showPersons){
     <div className ="Cockpit">
       <h1>{props.appTitle}</h1>
       <p className ={assignClass.join(' ')}>This is really working</p>
-      <button className ={btnClass} onClick ={props.nameChangeHandler}>Toggle Persons</button>
+      <button ref = {toggleBtnRef} className ={btnClass} onClick ={props.nameChangeHandler}>Toggle Persons</button>
     </div>
   )
 }
